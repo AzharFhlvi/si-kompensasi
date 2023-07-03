0deac60fbdf35deda0c835ba93830e82cab195da
AzharFhlvi <azharfahlevi.miraj@gmail.com>
2023-07-03 15:19:15 +0800

    make model Kompensasi mcr

---
+++ app/Http/Controllers/KompensasiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kompensasi;
+use Illuminate\Http\Request;
+
+class KompensasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kompensasi  $kompensasi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kompensasi $kompensasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kompensasi  $kompensasi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kompensasi $kompensasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kompensasi  $kompensasi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kompensasi $kompensasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kompensasi  $kompensasi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kompensasi $kompensasi)
+    {
+        //
+    }
+}
