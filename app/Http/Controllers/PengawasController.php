01975f9b483f14bcf2a21497cab08f455a167651
AzharFhlvi <azharfahlevi.miraj@gmail.com>
2023-07-03 15:20:51 +0800

    make model Pengawas mcr

---
+++ app/Http/Controllers/PengawasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengawas;
+use Illuminate\Http\Request;
+
+class PengawasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pengawas  $pengawas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pengawas $pengawas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pengawas  $pengawas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pengawas $pengawas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pengawas  $pengawas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pengawas $pengawas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pengawas  $pengawas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pengawas $pengawas)
+    {
+        //
+    }
+}
