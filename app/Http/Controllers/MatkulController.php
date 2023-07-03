b790b06a842f39228eea24d0c1af8d3a26afc18e
AzharFhlvi <azharfahlevi.miraj@gmail.com>
2023-07-03 15:06:16 +0800

    make model Matkul mcr

---
+++ app/Http/Controllers/MatkulController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Matkul;
+use Illuminate\Http\Request;
+
+class MatkulController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Matkul  $matkul
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Matkul $matkul)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Matkul  $matkul
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Matkul $matkul)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Matkul  $matkul
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Matkul $matkul)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Matkul  $matkul
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Matkul $matkul)
+    {
+        //
+    }
+}
